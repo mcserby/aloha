cd5a5e1a639d5fb2277845b364fbf2faed19b81a
mcserby <github@1>
2021-04-06 09:53:24 +0300

    added EvaluationOver page

---
+++ src/components/EvaluationOver.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="base-container thank-you">
+    <h3 class="message">
+      🎉 Current evaluation phase is over. Keep an eye out for new internships! 🎉
+    </h3>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'EvaluationOver',
+}
+</script>
+
+<style lang="scss" scoped>
+  .thank-you {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+
+    .message {
+      background: #4ECCA3;
+      padding: 2em;
+      border-radius: 15px;
+    }
+  }
+</style>
